aa8ff16d3909820e70ccc0a759ec5c1ea072670e
Christian Meier <meier.kristian@gmail.com>
2024-05-08 07:13:45 +0000

    separated steam and ASA game

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 #exit on error
-set -e
+#set -e
+
+#if [[ -n $NOTIFY_SOCKET ]] ; then
+#    systemd-notify --status "Installing/Updating ..."
+#fi
 
 # Install or update ASA server + verify installation
-/opt/steamcmd/steamcmd.sh +force_install_dir /opt/arkserver +login anonymous +app_update ${ASA_APPID} validate +quit
+#/opt/steamcmd/steamcmd.sh +force_install_dir /opt/arkserver +login anonymous +app_update ${ASA_APPID} validate +quit
 
 # Remove unnecessary files (saves 6.4GB.., that will be re-downloaded next update)
-if [[ -n "${REDUCE_IMAGE_SIZE}" ]]; then 
-    rm -rf /opt/arkserver/ShooterGame/Binaries/Win64/ArkAscendedServer.pdb
-    rm -rf /opt/arkserver/ShooterGame/Content/Movies/
-fi
+#if [[ -n "${REDUCE_IMAGE_SIZE}" ]]; then 
+#    rm -rf /opt/arkserver/ShooterGame/Binaries/Win64/ArkAscendedServer.pdb
+#    rm -rf /opt/arkserver/ShooterGame/Content/Movies/
+#fi
 
 #Create file for showing server logs
 mkdir -p "${LOG_FILE%/*}" && echo "" > "${LOG_FILE}"
@@ -20,10 +24,17 @@ echo "" > "${PID_FILE}"
 #manager start &
 
 # Register SIGTERM handler to stop server gracefully
-trap "manager stop --saveworld" SIGTERM
+#trap "manager stop --saveworld" SIGTERM
 
 # Start tail process in the background, then wait for tail to finish.
 # This is just a hack to catch SIGTERM signals, tail does not forward
 # the signals.
-tail -F "${LOG_FILE}" &
-wait $!
+#tail -F "${LOG_FILE}" &
+#wait $!
+
+if [[ -n $NOTIFY_SOCKET ]] ; then
+    systemd-notify --ready --status "Steam is ready..."
+fi
+
+#trap : TERM INT 
+sleep infinity #& wait
---
+++ scripts/steam.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+
+# Install or update ASA server + verify installation
+/opt/steamcmd/steamcmd.sh +force_install_dir /opt/server +login anonymous +app_update ${APPID} validate +quit
+
+# Remove unnecessary files (saves 6.4GB.., that will be re-downloaded next update)
+if [[ -n "${ARK_REDUCE_STORAGE_SIZE}" ]]; then 
+    rm -rf /opt/server/ShooterGame/Binaries/Win64/ArkAscendedServer.pdb
+    rm -rf /opt/server/ShooterGame/Content/Movies/
+fi
+
+# when running as systemd then notify when ready
+if [[ -n $NOTIFY_SOCKET ]] ; then
+    systemd-notify --ready --status "Steam is ready..."
+fi
+
+trap : TERM INT 
+sleep infinity & wait
